bc7bb3297bcab0321b013060d9ab804135eaa26a
Morten Nissen <mail@hjemmesidekongen.dk>
2020-01-14 08:27:11 +0100

    Webform

--- web/themes/custom/custom_theme/src/javascripts/app.js
+++ web/themes/custom/custom_theme/src/javascripts/app.js
@@ -9,6 +9,14 @@ jQuery(function ($) {
     .find('ul, li, a')
     .removeClass();
 
+  $('.sidr-toggle--right').sidr({
+    name: 'sidr-main',
+    side: 'right',
+    renaming: false,
+    body: '.layout__wrapper',
+    source: '.sidr-source-provider'
+  });
+
   // Enable tooltips.
   $('[data-toggle="tooltip"]').tooltip();
 });
