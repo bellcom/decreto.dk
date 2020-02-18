9f86e8321a1270ef501104a866d4648327b976f7
Morten Nissen <mail@hjemmesidekongen.dk>
2020-02-18 12:58:15 +0100

    FAQ

--- web/themes/custom/custom_theme/src/javascripts/faq-items.js
+++ web/themes/custom/custom_theme/src/javascripts/faq-items.js
@@ -1,17 +1,15 @@
-(function() {
-  const handleToggle = (event) => {
-    var target = event.target;
-    var parentNode = target.closest('.faq-item');
+const handleToggle = (event) => {
+  var target = event.target;
+  var parentNode = target.closest('.faq-item');
 
-    parentNode.classList.toggle('open');
-  };
+  parentNode.classList.toggle('open');
+};
 
 // Add eventListeners.
-  var toggles = document.querySelectorAll('.js-faq-item-toggle');
+var toggles = document.querySelectorAll('.js-faq-item-toggle');
 
-  for (var i = 0; i < toggles.length; i++) {
-    var item = toggles[i];
+for (var i = 0; i < toggles.length; i++) {
+  var item = toggles[i];
 
-    item.addEventListener('click', handleToggle);
-  }
-})();
+  item.addEventListener('click', handleToggle);
+}
